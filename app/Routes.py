7caca7d8ae0d4d4405e1e1d16d73180a4d2d889a
nourister <nouristerjuma@gmail.com>
2024-06-06 16:59:05 +0300

    inspiredaily

---
+++ app/Routes.py
@@ -0,0 +1,15 @@
+# from app import app
+# from flask import render_template
+
+
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
+
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact.html')
+
+# @app.route('/quotes')
+# def quotes():
+#     quotes = quotes
